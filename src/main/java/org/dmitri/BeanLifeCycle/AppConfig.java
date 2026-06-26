2aebe5900c88f5f8431e0958060a965b7d606ae7
dockerstar <67378512+dockerstar@users.noreply.github.com>
2026-06-26 10:09:02 +0300

    lessons BeanLC, create practic (#4)

---
+++ src/main/java/org/dmitri/BeanLifeCycle/AppConfig.java
@@ -0,0 +1,19 @@
+package org.dmitri.BeanLifeCycle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public AppLogger appLogger() {
+        return new AppLogger();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public OperationContext operationContext() {
+        return new OperationContext();
+    }
+}
